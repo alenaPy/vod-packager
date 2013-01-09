e419cedf5e0f6d35ed21f7e419aa30808b1dd441
emiliano.billi <emiliano.billi@gmail.com>
2013-01-09 15:34:49 +0000

    Commit JS

---
+++ Packager/Templates/js/item.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+	// Initializing accordion
+	$('.accordion').accordion({defaultOpen: ''});
+	// Adding events
+	//element.onclick = doSomething
+	// package_group select
+	$('#package_group').change(function() {
+		//alert('Value change to ' + $(this).attr('value'));
+		var opt = $("#package_group").val();
+		if (opt == "new") {
+			$('#new_package_group').click();
+		}
+	});
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '40%',
+		height		: '50%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
+
+function actionUploadImageRenditions(e) {
+	window.location = "/vod/image_renditions/upload/" + $("#item_id").val();
+}
+
+function actionExportItem() {
+	if (validateItem()) {
+		var jsonObj = {'item_id':$('#item_id').val(),'selected_customers':$('#customers_for_export').val(),'package_group':$('#package_group').val()};
+		
+		/*
+		var foo = []; 
+		$('#customers_for_export :selected').each(function(i, selected){ 
+			foo[i] = $(selected).text(); 
+		});
+		alert(foo);
+		*/
+		
+		//alert(JSON.stringify(jsonObj));
+		Dajaxice.Packager_app.export_item(clbkExportItem, jsonObj);
+	}
+}
+
+function clbkExportItem(data) {
+	alert(data.message);
+}
+
+function clbkNewPackageGroup(data) {
+	json_data = json_parse(data);
+	// Closing fancybox window
+	parent.$.fancybox.close();
+	// Add new option to package group select
+	$("#package_group").append("<option value='"+json_data.id+"'>"+json_data.name+"</option>");
+	// Select created option
+	$('select').val(json_data.id);
+	// Go to top
+	$('html, body').animate({ scrollTop: 0 }, 0);
+}
+
+function validateItem() {
+	return true;
+}
+
+/* DAJAXICE: Funciones de ejemplo, por favor no borrar! */
+
+function dajaxice_example() {
+	Dajaxice.Packager_app.dajaxice_example(my_callback_example_1);
+}
+
+function dajaxice_args_example() {
+	var jsonObj = {'text':$('#text').val()};
+	Dajaxice.Packager_app.args_example(my_callback_example_2, jsonObj);
+}
+
+function my_callback_example_1(data) {
+	alert(data.message);
+}
+
+function my_callback_example_2(data) {
+	alert(data.message);
+}
